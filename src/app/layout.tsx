c7fe643c7bb1119e0dbfdf05bb09a2ecaedf26c9
X58okYW8B54e <P4v8AU7pe87R@users.noreply.github.com>
2024-09-05 14:08:15 +0900

    update: blog title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog with`,
-  description: `A statically generated blog example using Next.js and $.`,
+  title: `Tech Blog`,
+  description: `Tech Blog`,
   metadataBase: new URL("https://blog-sample-9c91d.web.app/"),
   openGraph: {
     images: [HOME_OG_IMAGE_URL],
