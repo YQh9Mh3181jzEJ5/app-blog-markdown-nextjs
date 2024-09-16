d395144a2b6f7169a0cb21445ccbaa78c5a042c8
X58okYW8B54e <113420721+YQh9Mh3181jzEJ5@users.noreply.github.com>
2024-09-16 16:15:18 +0900

    fix posts margin

--- src/app/_components/more-stories.tsx
+++ src/app/_components/more-stories.tsx
@@ -11,7 +11,7 @@ export function MoreStories({ posts }: Props) {
       {/* <h2 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
         Tech Articles
       </h2> */}
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-6 lg:grid-cols-3 gap-4">
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
